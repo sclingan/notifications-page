30b02b00d81e1d70a9b97235082c307cdb64c641
Scott Clingan <sclingan1@gmail.com>
2023-11-06 14:30:12 -0600

    Adding basic css

--- src/section.tsx
+++ src/section.tsx
@@ -4,28 +4,28 @@ function Section() {
 
     return (
       <ol>
-          Mark Webber reacted to your recent post My first tournament today!
-          1m ago
+          <li>Mark Webber reacted to your recent post My first tournament today!
+          1m ago</li>
 
-          Angela Gray followed you
-          5m ago
+          <li>Angela Gray followed you
+          5m ago</li>
 
-          Jacob Thompson has joined your group Chess Club
-          1 day ago
+          <li>Jacob Thompson has joined your group Chess Club
+          1 day ago</li>
 
-          Rizky Hasanuddin sent you a private message
+          <li>Rizky Hasanuddin sent you a private message
           5 days ago
           Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
-          I'm already having lots of fun and improving my game.
+          I'm already having lots of fun and improving my game.</li>
 
-          Kimberly Smith commented on your picture
-          1 week ago
+          <li>Kimberly Smith commented on your picture
+          1 week ago</li>
 
-          Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
-          2 weeks ago
+          <li>Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
+          2 weeks ago</li>
 
-          Anna Kim left the group Chess Club
-          2 weeks ago
+          <li>Anna Kim left the group Chess Club
+          2 weeks ago</li>
       </ol>
     )
   }
