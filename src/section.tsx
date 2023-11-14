5fd7adb4ba9e1cf3f7a3278357efbb2c1e4d818c
Scott Clingan <sclingan1@gmail.com>
2023-11-14 14:06:47 -0600

    Adding unread state to posts

--- src/section.tsx
+++ src/section.tsx
@@ -13,13 +13,13 @@ function Section() {
          <ol>
              <li className='unread'>
               <img src={mark} alt=""></img>
-              <p className='post'><span className='user-name'>Mark Webber</span> reacted to your recent post My first tournament today!</p>
+              <p className='post'><span className='user-name'>Mark Webber</span> reacted to your recent post My first tournament today!<div className='dot'></div></p>
               <p className='post-time'>1m ago</p>
              </li>
 
              <li className='unread'>
               <img src={angela} alt=""></img>
-              <p className='post'><span className='user-name'>Angela Gray</span> followed you</p>
+              <p className='post'><span className='user-name'>Angela Gray</span> followed you<div className='dot'></div></p>
               <p className='post-time'>5m ago</p>
              </li>
 
@@ -31,7 +31,7 @@ function Section() {
 
              <li>
               <img src={rizky} alt=""></img>
-              <p className='post'><span className='user-name'>Rizky Hasanuddin</span> sent you a private message</p>
+              <p className='post'><span className='user-name'>Rizky Hasanuddin</span> sent you a private message<div className='dot'></div></p>
               <p className='post-time'>5 days ago</p>
               <p className='post-comments'>Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
                 I'm already having lots of fun and improving my game.</p>
@@ -39,19 +39,19 @@ function Section() {
 
              <li>
               <img src={kimberly} alt=""></img>
-              <p className='post'><span className='user-name'>Kimberly Smith</span> commented on your picture</p>
+              <p className='post'><span className='user-name'>Kimberly Smith</span> commented on your picture<div className='dot'></div></p>
               <p className='post-time'>1 week ago</p>
              </li>
 
              <li>
               <img src={nathan} alt=""></img>
-              <p className='post'><span className='user-name'>Nathan Peterson</span> reacted to your recent post 5 end-game strategies to increase your win rate</p>
+              <p className='post'><span className='user-name'>Nathan Peterson</span> reacted to your recent post 5 end-game strategies to increase your win rate<div className='dot'></div></p>
               <p className='post-time'>2 weeks ago</p>
               </li>
 
              <li>
               <img src={anna} alt=""></img>
-              <p className='post'><span className='user-name'>Anna Kim</span> left the group Chess Club</p>
+              <p className='post'><span className='user-name'>Anna Kim</span> left the group Chess Club<div className='dot'></div></p>
               <p className='post-time'>2 weeks ago</p>
              </li>
          </ol>
