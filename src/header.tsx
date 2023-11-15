43b600154cd2b3c5d4ffa1bb72eb02d67c2df9d2
Scott Clingan <sclingan1@gmail.com>
2023-11-15 14:27:47 -0600

    Added state and functions

--- src/header.tsx
+++ src/header.tsx
@@ -2,12 +2,29 @@ import { useState } from 'react'
 
 
 function Header() {
-  const [unread, setUnRead] = useState(3)
+  const [unread, setRead] = useState(0);
+  const [count, setCount] = useState(3);
+  function mark_Read() {
+    // All new msg are marked unread
+    // keep running count of unread msg and display in variable
+    let msg = document.getElementsByClassName('unread');
+    setCount(msg.length);
+    // onClick of li, change state to read: remove background color, remove red dot
+    // onClick of button mark all as read
+    for(let i = 0; i < count; i++) {
+      if(msg[i].className === 'unread') {
+        msg[i].classList.remove('unread');
+        msg[i].classList.add('test');
+      }
+    }
+    
+    console.log(msg, typeof(msg), msg.item(count -1));
+  }
     
     return (
       <header>
-         <h2>Notifications <span className='notification'>{unread}</span></h2>
-         <button>Mark all as read</button>
+         <h2>Notifications <span className='notification'>{count}</span></h2>{/*Fix unread notification*/}
+         <button onClick={mark_Read}>Mark all as read</button>
       </header>
     )
   }
