1e2e490d6c08339d8fe08beb325605298c637144
Scott Clingan <sclingan1@gmail.com>
2023-11-20 14:26:23 -0600

    Added mark as read functionality

--- src/header.tsx
+++ src/header.tsx
@@ -8,17 +8,15 @@ function Header() {
     // All new msg are marked unread
     // keep running count of unread msg and display in variable
     let msg = document.getElementsByClassName('unread');
+    let icon = document.getElementsByClassName('dot');
     setCount(msg.length);
     // onClick of li, change state to read: remove background color, remove red dot
     // onClick of button mark all as read
-    for(let i = 0; i < count; i++) {
-      if(msg[i].className === 'unread') {
-        msg[i].classList.remove('unread');
-        msg[i].classList.add('test');
-      }
-    }
+    const msgArr = Array.from(msg);
+    msgArr.map(e => e.classList.remove('unread'));  
+    const dotArr = Array.from(icon);
+    dotArr.map(e => e.classList.remove('dot'));
     
-    console.log(msg, typeof(msg), msg.item(count -1));
   }
     
     return (
--- src/section.tsx
+++ src/section.tsx
@@ -30,29 +30,29 @@ function Section() {
               <p className='post-time'>1 day ago</p>
              </li>
 
-             <li className='test'>
+             <li>
               <img src={rizky} alt=""></img>
-              <p className='post'><span className='user-name'>Rizky Hasanuddin</span> sent you a private message<span className='dot'></span></p>
+              <p className='post'><span className='user-name'>Rizky Hasanuddin</span> sent you a private message</p>
               <p className='post-time'>5 days ago</p>
               <p className='post-comments'>Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
                 I'm already having lots of fun and improving my game.</p>
               </li>
 
-             <li className='test'>
+             <li>
               <img src={kimberly} alt=""></img>
-              <p className='post'><span className='user-name'>Kimberly Smith</span> commented on your picture<span className='dot'></span></p>
+              <p className='post'><span className='user-name'>Kimberly Smith</span> commented on your picture</p>
               <p className='post-time'>1 week ago</p>
              </li>
 
-             <li className='test'>
+             <li>
               <img src={nathan} alt=""></img>
-              <p className='post'><span className='user-name'>Nathan Peterson</span> reacted to your recent post 5 end-game strategies to increase your win rate<span className='dot'></span></p>
+              <p className='post'><span className='user-name'>Nathan Peterson</span> reacted to your recent post 5 end-game strategies to increase your win rate</p>
               <p className='post-time'>2 weeks ago</p>
               </li>
 
-             <li className='test'>
+             <li>
               <img src={anna} alt=""></img>
-              <p className='post'><span className='user-name'>Anna Kim</span> left the group Chess Club<span className='dot'></span></p>
+              <p className='post'><span className='user-name'>Anna Kim</span> left the group Chess Club</p>
               <p className='post-time'>2 weeks ago</p>
              </li>
          </ol>
